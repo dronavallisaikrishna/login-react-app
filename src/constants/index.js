628ac58e737336128ea85f97ba19ba859cfcf95f
dronavalli.saikrishna <dronavalli.saikrishna@gmail.com>
2019-07-17 14:51:12 +0530

    Changes to URLS asper the deployment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://login-spring-boot-app.herokuapp.com';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'https://login-react-app.herokuapp.com/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
